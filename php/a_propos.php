39427546e0aeddbf3f02205faacb6ad687c00b4b
Ch4ise <benoit.bronsard@gmail.com>
2021-12-16 14:56:17 +0100

    ADD A_Propos + Modifs

--- php/a_propos.php
+++ php/a_propos.php
@@ -1,1 +1,22 @@
-A Propos
+<article id="accueil-article">
+    <div class='ap-header'>
+        <div>
+            <h1>Benoît Bronsard</h1>
+            <h2>- Etudiant en BTS SIO -</h2>
+        </div>
+    </div>
+    <div class='accueil-container'>
+        <div class='accueil-1st-container'>
+            <h1>Présentation</h1>
+            <p>abbbbbbbbbbabbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbababababababababababababababababababababababababababababababababababababababababababababababaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+        <div class='accueil-container-left'>
+            <span><h2>A propos de Moi</h2></span>
+            <p>Actuellement en 1ère année de BTS SIO, je suis à la recherche d'un stage.</p>
+            <p>Attiré par l’informatique et les nouvelles technologies depuis mon enfance, je souhaite me diriger vers la Cybersécurité. C’est un enjeu majeur de nos jours pour les entreprises et atteindre cet objectif se fera par l’acquisition des savoir-faire d’un vrai professionnel.</p>
+        </div>
+        <div class='accueil-container-right'>
+        </div>
+    </div>
+
+</article>
