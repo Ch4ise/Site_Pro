94f4a14380a4534dc11954c5b052b0c92ee00f31
Ch4ise <benoit.bronsard@gmail.com>
2021-12-16 17:04:17 +0100

    Add 3 Containers to AP

--- php/a_propos.php
+++ php/a_propos.php
@@ -5,17 +5,28 @@
             <h2>- Etudiant en BTS SIO -</h2>
         </div>
     </div>
-    <div class='accueil-container'>
-        <div class='accueil-1st-container'>
+    <div class='ap-container'>
+        <div class='ap-2nd-container'>
             <h1>Présentation</h1>
-            <p>abbbbbbbbbbabbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbbbb</p>
+            <p> blablablabla, blablablabla, blablablabla, </p>
         </div>
-        <div class='accueil-container-left'>
-            <span><h2>A propos de Moi</h2></span>
-            <p>Actuellement en 1ère année de BTS SIO, je suis à la recherche d'un stage.</p>
-            <p>Attiré par l’informatique et les nouvelles technologies depuis mon enfance, je souhaite me diriger vers la Cybersécurité. C’est un enjeu majeur de nos jours pour les entreprises et atteindre cet objectif se fera par l’acquisition des savoir-faire d’un vrai professionnel.</p>
-        </div>
-        <div class='accueil-container-right'>
+        <div class='ap-3rd-container'>
+            <div class='ap-3rd-left'>
+                <h2> Centres d'intérêts </h2>
+                <ul>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                </ul>
+            </div>
+            <div class='ap-3rd-right'>
+                <h2> Parcours Scolaire</h2>
+                <ul>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                </ul>
+            </div>
         </div>
     </div>
 
