fb2be038d61d7e24c181a967551ac81ef3753ca7
Ch4ise <90392391+Ch4ise@users.noreply.github.com>
2021-12-23 00:18:14 +0100

    MODIF contact

--- php/contact.php
+++ php/contact.php
@@ -1,28 +1,28 @@
 <?php
 
-    echo("<article id='form-article'>
-            <div class='form-header'>
-                <h1>Contactez-moi</h1>
-            </div>");
+    echo("<article id='form-article'>");
 
     echo("<div class='contact-container'>
-            <div class='contact-container-left'>
-                <h2>N'hésitez pas à me contacter !</h2>
-            <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2600.3413612478207!2d-0.3984980841501391!3d49.32675567556768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a5f010bac41bb%3A0x2cb1a5fb1ceef5aa!2s16%20Rue%20de%20la%20Passe%20d&#39;Aval%2C%2014750%20Saint-Aubin-sur-Mer!5e0!3m2!1sfr!2sfr!4v1640183326258!5m2!1sfr!2sfr' width='80%' height='80%' style='border:0;'' allowfullscreen='' loading='lazy'></iframe>
-            </div>")  ; 
+                <div class='contact-container-left'>
+                    <h2>N'hésitez pas à me contacter !</h2>
+                    <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2600.3413612478207!2d-0.3984980841501391!3d49.32675567556768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480a5f010bac41bb%3A0x2cb1a5fb1ceef5aa!2s16%20Rue%20de%20la%20Passe%20d&#39;Aval%2C%2014750%20Saint-Aubin-sur-Mer!5e0!3m2!1sfr!2sfr!4v1640183326258!5m2!1sfr!2sfr' width='80%' height='80%' style='border:0;'' allowfullscreen='' loading='lazy'></iframe>
+                </div>")  ; 
 
-    echo("<div class='contact-container-right>
+    echo(   "<div class='contact-container-right'>
                 <form action='' method='POST'>
-                    <p>Nom*</p>
-                    <input type='text' class='contact-square' name='nom' placeholder='Entrez votre nom...' required='required'>
-                    <p>E-mail*</p>
-                    <input type='mail' class='contact-square' name='nom' placeholder='Entrez votre e-mail...' required='required'>
-                    <p>Objet*</p>
-                    <input type='text' class='contact-square' name='nom' placeholder='Entrez votre objet...' required='required'>
-                </form>
-                <div class='h-captcha' data-sitekey='ccf9b1e1-9657-4a46-a4fa-92d88f3405e7'></div>
+                        <p>Nom*</p>
+                        <input type='text' class='contact-square' name='nom' placeholder='Entrez votre nom...' required='required'>
+                        <p>E-mail*</p>
+                        <input type='mail' class='contact-square' name='nom' placeholder='Entrez votre e-mail...' required='required'>
+                        <p>Objet*</p>
+                        <input type='text' class='contact-square' name='nom' placeholder='Entrez votre objet...' required='required'>
+                        <p>Contenu du mail</p>
+                        <textarea class='contact-message' name='comment' placeholder='Entrez votre commentaire'></textarea><br>
+                    </form>
+                    <div class='h-captcha' data-sitekey='ccf9b1e1-9657-4a46-a4fa-92d88f3405e7'></div>
+            </div>
         </div>
-        </div>");
+    </article>");
 
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
